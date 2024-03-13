0ec8961372db373bc69581f267681b6cfcd86a45
KarimAldeen <MhmadKarimAldeen@gmail.com>
2024-03-13 06:18:47 +0300

    Done

--- src/Pages/Service/ServiceBenefit/useTableColumn.tsx
+++ src/Pages/Service/ServiceBenefit/useTableColumn.tsx
@@ -3,7 +3,7 @@ import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useDeleteSubService } from "../../../api/subServices";
 import { usePageState } from "../../../lib/state mangment/dist/LayoutPagestate";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ColumnsImage from "../../../Components/Columns/ColumnsImage";
 import { convert_language_array_to_local } from "../../../utils/language/ConvertObjectToLocalLanguage";
 import Actions from "../../../Components/Ui/tables/Actions";
@@ -15,10 +15,11 @@ const useTableColumns :any = () => {
   const [t] = useTranslation();
   const deleteMutation = useDeleteSubService()
   const navigate = useNavigate()
+  const {id} = useParams()
   const { setObjectToEdit, objectToEdit } = usePageState()
   function handelEdit(row:any){
     setObjectToEdit(row)
-     navigate(`edit`)
+     navigate(`benefit/${row?.id}`)
   }
 
   return useMemo(
